b2b2246ec7d41b66a4061bc8f55542d60aa47f5d
Changhee Choi <changhee.dev@gmail.com>
2019-12-03 18:29:18 +0900

    CustomUserDetailsService 구현

---
+++ src/main/java/com/example/springsecurityjwt/controller/HelloController.java
@@ -0,0 +1,13 @@
+package com.example.springsecurityjwt.controller;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class HelloController {
+
+    @GetMapping("/hello")
+    public String sayHello() {
+        return "Hello World";
+    }
+}
---
+++ src/main/java/com/example/springsecurityjwt/service/CustomUserDetailsService.java
@@ -0,0 +1,38 @@
+package com.example.springsecurityjwt.service;
+
+
+import com.example.springsecurityjwt.model.User;
+import com.example.springsecurityjwt.dto.UserDetailsDTO;
+import com.example.springsecurityjwt.repository.UserRepository;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
+import org.springframework.stereotype.Service;
+
+@Service
+public class CustomUserDetailsService implements UserDetailsService {
+
+    private UserRepository userRepository;
+
+    public CustomUserDetailsService(UserRepository userRepository) {
+        this.userRepository = userRepository;
+    }
+
+    @Override
+    public UserDetails loadUserByUsername(String username) throws UsernameNotFoundException {
+        User user = userRepository.findByEmail(username);
+
+        if(user == null)
+            throw new UsernameNotFoundException("등록되지 않은 회원입니다.");
+
+        UserDetailsDTO userDetailsDTO = new UserDetailsDTO().builder()
+                .id(user.getId())
+                .name(user.getName())
+                .username(user.getUsername())
+                .password(user.getPassword())
+                .authorities(user.getAuthorities())
+                .build();
+
+        return userDetailsDTO;
+    }
+}
