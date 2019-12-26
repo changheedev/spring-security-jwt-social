8d7013cb648cf6f1079a8b13780d9061eba50a5f
Changhee Choi <changhee.dev@gmail.com>
2019-12-26 20:00:03 +0900

    CORS 설정 추가

---
+++ server/src/main/java/com/example/springsecurityjwt/authentication/oauth2/MvcConfigurer.java
@@ -0,0 +1,24 @@
+package com.example.springsecurityjwt.config;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.http.HttpMethod;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+@Configuration
+public class MvcConfigurer implements WebMvcConfigurer {
+
+    @Override
+    public void addCorsMappings(CorsRegistry registry) {
+        registry.addMapping("/**")
+                .allowedOrigins("http://localhost:3000")
+                .allowedMethods(
+                        HttpMethod.GET.name(),
+                        HttpMethod.HEAD.name(),
+                        HttpMethod.POST.name(),
+                        HttpMethod.PUT.name(),
+                        HttpMethod.DELETE.name())
+                .maxAge(3600)
+                .allowCredentials(true);
+    }
+}
