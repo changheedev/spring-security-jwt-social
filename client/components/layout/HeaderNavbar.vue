50dd363a6a4c4c239b75d70e750319947345c523
Changhee Choi <changhee.dev@gmail.com>
2019-12-29 22:25:50 +0900

    로그아웃 버튼 추가

--- client/components/layout/HeaderNavbar.vue
+++ client/components/layout/HeaderNavbar.vue
@@ -2,14 +2,36 @@
   <b-navbar type="light" variant="light" fixed="top">
     <b-navbar-brand href="/">Spring Social</b-navbar-brand>
     <b-navbar-nav class="ml-auto">
-      <b-nav-item href="/login" right>Sign in</b-nav-item>
+      <b-nav-item-dropdown
+        id="my-nav-dropdown"
+        :text="loggedName"
+        toggle-class="nav-link-custom"
+        right
+        v-if="loggedName"
+      >
+        <b-dropdown-item @click="logout()">Logout</b-dropdown-item>
+      </b-nav-item-dropdown>
+      <b-nav-item :to="`/login?redirect_uri=${$route.path}`" right v-else
+        >Sign in</b-nav-item
+      >
     </b-navbar-nav>
   </b-navbar>
 </template>
 
 <script>
-export default {};
+import { mapGetters } from "vuex";
+export default {
+  computed: mapGetters({
+    loggedName: "loggedName"
+  }),
+  methods: {
+    logout() {
+      this.$axios.post("/api/authorize/logout").then(() => {
+        this.$store.commit("clearLoggedName");
+      });
+    }
+  }
+};
 </script>
 
-<style lang="scss" scoped>
-</style>
+<style lang="scss" scoped></style>
