efe4e33ae7948fc3032844af3146c11fd487af39
Changehee Choi <changhee.dev@gmail.com>
2020-01-07 22:48:42 +0900

    OAuth2Account 엔티티에 토큰 정보를 함께 저장하도록 변경

--- server/src/main/java/com/example/springsecurityjwt/authentication/oauth2/account/OAuth2Account.java
+++ server/src/main/java/com/example/springsecurityjwt/authentication/oauth2/account/OAuth2Account.java
@@ -8,6 +8,7 @@ import lombok.Getter;
 import lombok.NoArgsConstructor;
 
 import javax.persistence.*;
+import java.time.LocalDateTime;
 
 @Entity
 @Table(name = "TBL_OAUTH_ACCOUNT")
@@ -15,16 +16,28 @@ import javax.persistence.*;
 @NoArgsConstructor(access = AccessLevel.PROTECTED)
 public class OAuth2Account extends BaseEntity {
 
-    @ManyToOne(fetch = FetchType.LAZY)
+    @OneToOne(fetch = FetchType.LAZY)
     @JoinColumn(name = "USER_ID")
     private User user;
     private String providerId;
     private String provider;
+    private String token;
+    private String refreshToken;
+    private LocalDateTime tokenExpiredAt;
 
     @Builder
-    public OAuth2Account(User user, String providerId, String provider) {
+    public OAuth2Account(User user, String providerId, String provider, String token, String refreshToken, LocalDateTime tokenExpiredAt) {
         this.user = user;
         this.providerId = providerId;
         this.provider = provider;
+        this.token = token;
+        this.refreshToken = refreshToken;
+        this.tokenExpiredAt = tokenExpiredAt;
+    }
+
+    public void updateToken(String token, String refreshToken, LocalDateTime tokenExpiredAt) {
+        this.token = token;
+        this.refreshToken = refreshToken;
+        this.tokenExpiredAt = tokenExpiredAt;
     }
 }
